f62b8f2dc602df8ddce3bb528ec3b6c605f9500a
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-07-08 20:38:37 -0500

    Update coach_answers_interface.rb

--- Ruby - 1 Programming Basics/coach_answers_interface.rb
+++ Ruby - 1 Programming Basics/coach_answers_interface.rb
@@ -1,6 +1,6 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
-puts "Good morning. What are you doing today?"
+puts "Good morning. What are you doing right now?"
 your_message = gets.chomp
 
 print coach_answer(your_message)
