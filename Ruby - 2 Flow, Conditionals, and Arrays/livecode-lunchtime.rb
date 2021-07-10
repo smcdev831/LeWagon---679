fd048ce09f8df44e95ec9087db34936006ac0b46
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-07-10 09:57:40 -0500

    Update livecode-lunchtime.rb

--- Ruby - 2 Flow, Conditionals, and Arrays/livecode-lunchtime.rb
+++ Ruby - 2 Flow, Conditionals, and Arrays/livecode-lunchtime.rb
@@ -2,10 +2,10 @@ hour = Time.now.hour
 
 if hour < 12
   puts "Good morning!
-elsif hour > 12
-  puts "Good afternoon!"
 elsif hour >= 20
   puts "Good night!"
+elsif hour > 12
+  puts "Good afternoon!"
 else
   puts "Lunch Time!"
 end
