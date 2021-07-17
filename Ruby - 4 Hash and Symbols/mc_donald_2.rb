1dee60040290e0e1c1d4c9f0d722c3b2b4987613
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-07-17 11:56:11 -0500

    Create mc_donald_2.rb

---
+++ Ruby - 4 Hash and Symbols/mc_donald_2.rb
@@ -0,0 +1,32 @@
+DISHES_CALORIES = {
+  "Hamburger" => 250,
+  "Cheese Burger" => 300,
+  "Big Mac" => 540,
+  "McChicken" => 350,
+  "French Fries" => 230,
+  "Salad" => 15,
+  "Coca Cola" => 150,
+  "Sprite" => 150
+}
+
+MEALS = {
+  "Happy Meal" => ["Cheese Burger", "French Fries", "Coca Cola"],
+  "Best Of Big Mac" => ["Big Mac", "French Fries", "Coca Cola"],
+  "Best Of McChicken" => ["McChicken", "Salad", "Sprite"]
+}
+
+def poor_calories_counter(burger, side, beverage)
+  DISHES_CALORIES[burger] + DISHES_CALORIES[side] + DISHES_CALORIES[beverage]
+end
+
+def calories_counter(orders)
+  total_calories = 0
+  orders.each do |order|
+    if DISHES_CALORIES.key?(order)
+      total_calories += DISHES_CALORIES[order]
+    else
+      total_calories += poor_calories_counter(MEALS[order][0], MEALS[order][1], MEALS[order][2])
+    end
+  end
+  return total_calories
+end
