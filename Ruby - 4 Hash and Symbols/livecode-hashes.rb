be1712bdb0f35d10384129c50e07d6846970d154
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-07-17 09:46:15 -0500

    Update livecode-hashes.rb

--- Ruby - 4 Hash and Symbols/livecode-hashes.rb
+++ Ruby - 4 Hash and Symbols/livecode-hashes.rb
@@ -2,6 +2,7 @@
 students = ["Peter", "Mary", "George", "Emma"]
 students_age = [24, 25, 22, 20]
 
+#Create
 #standard
 students_age = {
 "Peter": 24,
@@ -29,3 +30,29 @@ students_age = {
 students_age.each do |name, age|
   puts "#{name}, #{age}"
 end
+
+
+#Read
+p students_age ["George"]
+p students_age [:Mary]
+
+
+#Update
+students_age["Peter"] = 30
+
+students_age = {
+"Peter": age: 24, location: mexico
+"Mary": 25,
+"George": 22,
+"Emma": 20
+}
+
+p students_age
+previous_info = students_age["Peter"]
+previous_info[:location] = "usa"
+p students_age
+
+
+#Delete
+students_age.delete("George")
+p students_age
