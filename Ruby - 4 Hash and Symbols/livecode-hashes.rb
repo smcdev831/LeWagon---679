9631c92b62f5f8fb38777a6289716246677dc9b2
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-07-17 09:37:53 -0500

    Update livecode-hashes.rb

--- Ruby - 4 Hash and Symbols/livecode-hashes.rb
+++ Ruby - 4 Hash and Symbols/livecode-hashes.rb
@@ -1,8 +1,29 @@
+#array
+students = ["Peter", "Mary", "George", "Emma"]
+students_age = [24, 25, 22, 20]
+
+#standard
 students_age = {
 "Peter": 24,
 "Mary": 25,
 "George": 22,
 "Emma": 20
+}
+
+#rocket
+students_age = {
+"Peter" => 24,
+"Mary" => 25,
+"George" => 22,
+"Emma" => 20
+}
+
+#symbol
+students_age = {
+:Peter => 24,
+:Mary => 25,
+:George => 22,
+:Emma => 20
 }
 
 students_age.each do |name, age|
