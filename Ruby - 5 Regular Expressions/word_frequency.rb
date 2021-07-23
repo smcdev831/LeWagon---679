292eba2736827faecc83983ce5a2bc757110032d
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-07-23 00:13:27 -0500

    Update word_frequency.rb

--- Ruby - 5 Regular Expressions/word_frequency.rb
+++ Ruby - 5 Regular Expressions/word_frequency.rb
@@ -1,19 +1,22 @@
 def most_common_words(file_name, stop_words_file_name, number_of_word)
-  # TODO: return hash of occurences of number_of_word most frequent words
   stop_words = load_stop_words(stop_words_file_name)
   counter_hash = Hash.new(0)
+  counter_hash = create_hash_from_file(file_name, stop_words, counter_hash)
+  counter = counter_hash.sort_by { |_, v| -v }
+  return counter[0...number_of_word].to_h
+end
+
+def create_hash_from_file(file_name, stop_words, counter_hash)
   File.open(file_name, "r").each_line do |line|
     line_array = line.split(/\W+/)
     line_array.each do |word|
       word = word.downcase
-      if not stop_words.include?(word)
-        counter_hash[word] += 1
-      end
+      counter_hash[word] += 1 unless stop_words.include?(word)
     end
   end
-  counter = counter_hash.sort_by {|_, v| -v}
-  return counter[0...number_of_word].to_h
+  return counter_hash
 end
+
 def load_stop_words(stop_words_file_name)
   stop_words = []
   File.open(stop_words_file_name, "r").each_line do |line|
@@ -21,4 +24,3 @@ def load_stop_words(stop_words_file_name)
   end
   stop_words
 end
-# puts most_common_words("../source.txt", "stop_words.txt", 3 )
