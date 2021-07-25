15732496e51a0aa8417a6088c796848b81ae20e2
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-07-25 01:28:41 -0500

    Create videogame.rb

---
+++ OOP - 1 Classes and Instances/videogame.rb
@@ -0,0 +1,11 @@
+class VideoGame
+  def initialize(platform, developer, type)
+    @platform = platform
+    @developer = developer
+    @type = type
+  end
+
+  def adding
+    puts "You added a new game"
+  end
+end
