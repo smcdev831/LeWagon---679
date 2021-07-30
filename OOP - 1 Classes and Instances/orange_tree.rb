7ff2afb81b9f73640183aee4eca6d072e55e3cd5
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-07-29 20:51:40 -0500

    Update orange_tree.rb

--- OOP - 1 Classes and Instances/orange_tree.rb
+++ OOP - 1 Classes and Instances/orange_tree.rb
@@ -1,3 +1,49 @@
 class OrangeTree
-  # TODO: Implement all the specs defined in the README.md :)
+  attr_reader :age, :height, :fruits
+
+  def initialize
+    @age = 0
+    @height = 0
+    @fruits = 0
+    @dead = false
+  end
+
+  def one_year_passes!
+    unless dead?
+      @age += 1
+      life_status
+      @height += 1 until @age == 10
+
+      fruit_production
+      life_status
+    end
+  end
+
+  def dead?
+    return @dead
+  end
+
+  def life_status
+    if @age <= 50
+      @dead = false
+    elsif @age < 50 && @age <= 100
+      @dead = [true, false].sample
+    else
+      @dead = true
+    end
+  end
+
+  def fruit_production
+    if @age >= 10 && @age < 15
+      @fruits = 200
+    elsif @age >= 5 && @age < 10
+      @fruits = 100
+    else
+      @fruits = 0
+    end
+  end
+
+  def pick_a_fruit
+    @fruits -= 1 if @fruits.positive? && alive?
+  end
 end
