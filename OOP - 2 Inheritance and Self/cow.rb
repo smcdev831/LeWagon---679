80fecf0333644fd4f18b7f2b5800ad6be27a71a7
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-07-31 12:05:08 -0500

    Create cow.rb

---
+++ OOP - 2 Inheritance and Self/cow.rb
@@ -0,0 +1,15 @@
+require_relative "animal"
+class Cow < Animal
+  attr_reader :milk
+  def initialize
+    super
+    @milk = 0
+  end
+  def talk
+    return 'moo'
+  end
+  def feed!
+    @milk += 2
+    super
+  end
+end
