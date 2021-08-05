76c0ab313a6432e8633622af1c8f558cd1dc813c
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-08-04 22:41:34 -0500

    Update app.rb

--- OOP - 3 Cookbook (Day 1)/app.rb
+++ OOP - 3 Cookbook (Day 1)/app.rb
@@ -1,6 +1,6 @@
-require_relative 'cookbook'    # You need to create this file!
-require_relative 'controller'  # You need to create this file!
-require_relative 'router'
+require 'cookbook'    # You need to create this file!
+require 'controller'  # You need to create this file!
+require 'router'
 
 csv_file   = File.join(__dir__, 'recipes.csv')
 cookbook   = Cookbook.new(csv_file)
