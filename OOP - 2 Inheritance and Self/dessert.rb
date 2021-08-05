f2dc2dd90e756783821ccfae5baf0a3449e31e86
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-08-05 09:34:33 -0500

    Create dessert.rb

---
+++ OOP - 2 Inheritance and Self/dessert.rb
@@ -0,0 +1,33 @@
+class Dessert
+  attr_reader :name, :calories
+
+  def initialize(name, calories)
+    @name = name
+    @calories = calories
+  end
+
+  def healthy?
+    @calories < 200
+  end
+
+  def delicious?
+    return true
+  end
+end
+
+class JellyBean < Dessert
+  attr_reader :flavor
+
+  def initialize(name, calories, flavor)
+    super(name, calories)
+    @flavor = flavor
+  end
+
+  def delicious?
+    if @flavor == "black licorice"
+      return false
+    else
+      return true
+    end
+  end
+end
