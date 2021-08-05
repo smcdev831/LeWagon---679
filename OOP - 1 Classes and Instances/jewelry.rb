ec7be87a9a73b69629ae95b6363033c63f69437c
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-08-05 09:34:24 -0500

    Create jewelry.rb

---
+++ OOP - 1 Classes and Instances/jewelry.rb
@@ -0,0 +1,12 @@
+class Jewelry
+  def initialize(size, material, price)
+    @size = size
+    @material = material
+    @price = price
+    @type = type
+  end
+
+  def adding
+    puts "You added a ${type}"
+  end
+end
