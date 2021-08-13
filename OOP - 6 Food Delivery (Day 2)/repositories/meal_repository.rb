483d4d039b135ed73c9a64cc0fc3d6528f604d41
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-08-13 09:20:42 -0500

    Update meal_repository.rb

--- OOP - 6 Food Delivery (Day 2)/repositories/meal_repository.rb
+++ OOP - 6 Food Delivery (Day 2)/repositories/meal_repository.rb
@@ -2,12 +2,10 @@ require "csv"
 require_relative "../models/meal"
 
 class MealRepository
-  attr_accessor :name, :id, :price
-
   def initialize(csv_file)
     @csv_file = csv_file
-    @next_id = 1
     @meals = []
+    @next_id = 1
     load_csv if File.exist?(@csv_file)
   end
 
@@ -28,6 +26,15 @@ class MealRepository
 
   private
 
+  def save_csv
+    CSV.open(@csv_file, "wb") do |csv|
+      csv << %w[id name price]
+      @meals.each do |meal|
+        csv << [meal.id, meal.name, meal.price]
+      end
+    end
+  end
+
   def load_csv
     csv_options = { headers: :first_row, header_converters: :symbol }
     CSV.foreach(@csv_file, csv_options) do |row|
@@ -36,14 +43,5 @@ class MealRepository
       @meals << Meal.new(row)
     end
     @next_id = @meals.last.id + 1 unless @meals.empty?
-  end
-
-  def save_csv
-    CSV.open(@csv_file, "wb") do |csv|
-      csv << ['id', 'name', 'price']
-      @meals.each do |meal|
-        csv << [meal.id, meal.name, meal.price]
-      end
-    end
   end
 end
