ceda03d0cb36c37bf32caa3b936a4ae5711716c9
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-08-13 09:20:17 -0500

    Update meals_controller.rb

--- OOP - 6 Food Delivery (Day 2)/controllers/meals_controller.rb
+++ OOP - 6 Food Delivery (Day 2)/controllers/meals_controller.rb
@@ -1,18 +1,16 @@
-require_relative "../views/meal_view"
+require_relative "../views/meals_view"
 require_relative "../models/meal"
 
 class MealsController
-  attr_accessor :id, :name, :price
-
   def initialize(meal_repository)
     @meal_repository = meal_repository
     @meals_view = MealsView.new
   end
 
   def add
-    name = @meals_view.ask_name
-    price = @meals_view.ask_price
-    meal = Meal.new(name: name, price: price.to_i)
+    name = @meals_view.ask_user_for(:name)
+    price = @meals_view.ask_user_for(:price).to_i
+    meal = Meal.new(name: name, price: price)
     @meal_repository.create(meal)
     display_meals
   end
