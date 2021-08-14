b604007626107da5ef97286bc0b13b55c3d2d810
Sharon Crabtree <73855060+smcdev831@users.noreply.github.com>
2021-08-14 09:22:38 -0500

    Update customer_repository.rb

--- OOP - 5 Food Delivery (Day 1)/repositories/customer_repository.rb
+++ OOP - 5 Food Delivery (Day 1)/repositories/customer_repository.rb
@@ -2,12 +2,10 @@ require "csv"
 require_relative "../models/customer"
 
 class CustomerRepository
-  attr_accessor :name, :id, :address
-
   def initialize(csv_file)
     @csv_file = csv_file
-    @next_id = 1
     @customers = []
+    @next_id = 1
     load_csv if File.exist?(@csv_file)
   end
 
@@ -28,23 +26,21 @@ class CustomerRepository
 
   private
 
-  def load_csv
-    csv_options = { headers: :first_row, header_converters: :symbol }
-    CSV.foreach(@csv_file, csv_options) do |row|
-      row[:name] = row[:name]
-      row[:id] = row[:id].to_i
-      row[:address] = row[:address]
-      @customers << Customer.new(row)
-    end
-    @next_id = @customers.last.id + 1 unless @customers.empty?
-  end
-
   def save_csv
     CSV.open(@csv_file, "wb") do |csv|
-      csv << ['id', 'name', 'address']
+      csv << %w[id name address]
       @customers.each do |customer|
         csv << [customer.id, customer.name, customer.address]
       end
     end
+  end
+
+  def load_csv
+    csv_options = { headers: :first_row, header_converters: :symbol }
+    CSV.foreach(@csv_file, csv_options) do |row|
+      row[:id] = row[:id].to_i
+      @customers << Customer.new(row)
+    end
+    @next_id = @customers.last.id + 1 unless @customers.empty?
   end
 end
